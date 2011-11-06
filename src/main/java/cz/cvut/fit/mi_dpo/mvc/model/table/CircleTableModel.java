1ced9ed0bd919762fb8a9cb86a3b8541d2c9c864
JeanVEGA <ondrusek.jan@gmail.com>
2011-11-06 22:25:03 +0100

    added table models, storage service prepared to return concrete shapes

---
+++ src/main/java/cz/cvut/fit/mi_dpo/mvc/model/table/CircleTableModel.java
@@ -0,0 +1,27 @@
+package cz.cvut.fit.mi_dpo.mvc.model.table;
+
+import javax.swing.table.AbstractTableModel;
+
+public class CircleTableModel extends AbstractTableModel {
+
+	private static final long serialVersionUID = 3849409360925265002L;
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/cz/cvut/fit/mi_dpo/mvc/model/table/SquareTableModel.java
@@ -0,0 +1,27 @@
+package cz.cvut.fit.mi_dpo.mvc.model.table;
+
+import javax.swing.table.AbstractTableModel;
+
+public class SquareTableModel extends AbstractTableModel {
+
+	private static final long serialVersionUID = 5479831356906203866L;
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
